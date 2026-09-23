4587d7541f17538bf791f3508a942db002ce9f0e
rit372004 <riteshranjan620012@gmail.com>
2026-09-23 11:20:09 -0700

    SearchRotatedSortedArray

--- DivideAndConquer/SearchRotatedSortedArray.java
+++ DivideAndConquer/SearchRotatedSortedArray.java
@@ -40,8 +40,7 @@ public class SearchRotatedSortedArray {
 
     public static void main(String[] args) {
         int arr[] = { 4, 5, 6, 7, 0, 1, 2 };
-        int target = 6;
-        int tarIdx = search(arr, target, 0, arr.length - 1);
+        int tarIdx = search(arr, 7, 0, arr.length - 1);
         System.out.println(tarIdx);
     }
 }
